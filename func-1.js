21a5897a8bce1508c6fef70bdd8c80a4c9a73206
Taeil Kwak <taeil2@gmail.com>
2020-04-20 14:43:36 -0500

    return year of birth

--- func-1.js
+++ func-1.js
@@ -1,5 +1,7 @@
 function createGreeting(name, age) {
-    return `Hi, my name is ${name} and I'm ${age} years old`;
+    const yearOfBirth = 2020 - age;
+    return `I was born in ${yearOfBirth}`;
+    // return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
 const greeting1 = createGreeting('Taeil', 85);
