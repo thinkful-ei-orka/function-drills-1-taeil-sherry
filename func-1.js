dec607fcf20c82950d6c72229e0c85a41c7e0801
Taeil Kwak <taeil2@gmail.com>
2020-04-20 14:42:19 -0500

    Create greeting function accepts name and age

--- func-1.js
+++ func-1.js
@@ -1,6 +1,6 @@
-function createGreeting{
-    return "Hi, my name is Taeil and I'm 85 years old";
+function createGreeting(name, age) {
+    return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Taeil', 85);
 console.log(greeting1);
