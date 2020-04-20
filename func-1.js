2b8ecdde2186a2a31695e05fd59b90c98721c016
Taeil Kwak <taeil2@gmail.com>
2020-04-20 14:40:48 -0500

    reconfigured the greeting function

--- func-1.js
+++ func-1.js
@@ -1,5 +1,6 @@
 function createGreeting{
-    console.log("Hi, my name is Taeil and I'm 85 years old");
+    return "Hi, my name is Taeil and I'm 85 years old";
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
