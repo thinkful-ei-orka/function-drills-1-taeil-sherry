c20c7a5200cba4620d843c4830f5c879ce914575
Taeil Kwak <taeil2@gmail.com>
2020-04-20 14:56:04 -0500

    made a few fixes

--- func-1.js
+++ func-1.js
@@ -2,9 +2,7 @@
 
 
 function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error("Age cannot be negative");
-  }
+
   return 2020 - age;
 }
 
@@ -12,18 +10,20 @@ function createGreeting(name, age) {
   if (typeof name === 'undefined' || typeof age === 'undefined') {
       throw new Error("Arguments not valid.");
   }
+  if (age < 0) {
+    throw new Error("Age cannot be negative");
+  }
   if (typeof name !== 'string' || typeof age !== 'number') {
-      throw new TypeError();
+      throw new TypeError('Name must be a string and age must be a number.');
   }
 
   const yearOfBirth = getYearOfBirth(age);
-  return `I was born in ${yearOfBirth}`;
-  // return `Hi, my name is ${name} and I'm ${age} years old`;
+  return `Hi, my name is ${name}, and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 try {
   const greeting1 = createGreeting('Taeil', 85);
   console.log(greeting1);
 } catch(e) {
-  console.log("It didn't work.");
+  console.log(e.message);
 }
